12a6572687a6f1a4b116df2fab712dacbf0071b6
samuel <samuelt12@gmail.com>
2021-02-15 11:24:36 -0700

    good errors

--- src/core/error/http-error.js
+++ src/core/error/http-error.js
@@ -1,9 +1,10 @@
-class HttpError extends Error {
+class HttpError {
     constructor(statusCode, message, name) {
-        super()
+        this.stack = (new Error(message)).stack;
         this.name = name ? name : "HttpError";
         this.message = message;
         this.statusCode = statusCode;
+
     }
 }
 
--- src/romans/roman.controller.js
+++ src/romans/roman.controller.js
@@ -2,6 +2,7 @@ const BadRequest = require('../core/error/bad-request')
 async function numToRom(req, res, next) {
     try {
         // Error handing
+
         throw new BadRequest('no number')
         console.log(req.params.number)
         res.send('hi')
